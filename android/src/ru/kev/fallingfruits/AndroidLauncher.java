57ed732ec0a2e6455608d91eddf6f8235556f897
Катрин <www.maleckiy33@gmail.com>
2018-10-04 14:32:37 +0300

    Создание проекта

---
+++ android/src/ru/kev/fallingfruits/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.kev.fallingfruits;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.kev.fallingfruits.FallingFruits;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FallingFruits(), config);
+	}
+}
